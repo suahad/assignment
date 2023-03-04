786f35d4bb5e37c9f93dd16c8b9385b969b1d1db
Souhad  Malaysha <97182230+suahad@users.noreply.github.com>
2023-03-04 11:22:18 +0200

    Update SuspiciousEventsServiceTest.java

--- src/test/java/com.freightos.suseventsdetector/service/SuspiciousEventsServiceTest.java
+++ src/test/java/com.freightos.suseventsdetector/service/SuspiciousEventsServiceTest.java
@@ -26,13 +26,9 @@ public class SuspiciousEventsServiceTest {
     public void captureUnauthorizedRequests() throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         List<UnauthorizedUser> expectedUnauthorizedResponse = mapper.readValue(new URL("file:src/test/resources/service/unauthorized_users_response.json"), new TypeReference<List<UnauthorizedUser>>(){});
-        List<UnauthorizedUser> actualUnauthorizedResponse = suspiciousEventsService.getUnauthorizedRequests("\"isAuthorized\": true", 2);
-        for(UnauthorizedUser user : actualUnauthorizedResponse){
-            System.out.println(user);
-                  }
-
-        //assertNotNull(expectedUnauthorizedResponse);
-        //assertNotNull(actualUnauthorizedResponse);
+        List<UnauthorizedUser> actualUnauthorizedResponse = suspiciousEventsService.getUnauthorizedRequests("\"isAuthorized\": true", 3);
+        assertNotNull(expectedUnauthorizedResponse);
+        assertNotNull(actualUnauthorizedResponse);
 
         assertEquals(mapper.readTree(mapper.writeValueAsString(expectedUnauthorizedResponse)), mapper.readTree(mapper.writeValueAsString(actualUnauthorizedResponse)));
     }
